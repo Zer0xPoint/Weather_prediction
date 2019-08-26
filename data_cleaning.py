6d76700cbdd6371ce47024d658169e03841f3574
zer0xpoint <liyizhou@me.com>
2019-08-26 22:30:38 +0800

    update data_cleaning now cleaned data is saved in /data/all_cleaned.csv

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,61 @@
 import pandas as pd
 
-data = pd.read_csv('../data/all.csv')
-print(data.head())
-# print(data)
+df = pd.read_csv('../data/all.csv')
+df['Date'] = df['DATE'].map(lambda x: x.split('T')[0]).apply(pd.to_datetime) + df['DATE'].map(
+    lambda x: x.split('T')[1]).apply(pd.to_timedelta)
+# df['Time'] = df['DATE'].map(lambda x: x.split('T')[1]).apply(pd.to_timedelta) + df['Date']
+# df['Date'] = df['Date'].map(lambda x: x.split('T')[0]).apply(pd.to_datetime)
+df['Temperature'] = df['TMP'].map(lambda x: int(x.split(',')[0]) / 10).apply(pd.to_numeric)
+df['Dewpoint'] = df['DEW'].map(lambda x: int(x.split(',')[0]) / 10).apply(pd.to_numeric)
+# df = df[['Date', 'Time', 'Temperature', 'Dewpoint']].set_index('Date')
+
+
+df = df[['Date', 'Temperature', 'Dewpoint']].set_index('Date')
+
+features = ['Date', 'Temperature', 'Dewpoint']
+
+# rows = tmp.shape[0]
+#
+# nth_prior_measurements = [None] * N + [tmp[feature][i - N] for i in range(N, rows)]
+#
+# col_name = '{}_{}'.format(feature, N)
+# tmp[col_name] = nth_prior_measurements
+
+# 删除不符合常理的数据
+for i, row in df.iterrows():
+    if any([
+        df.loc[i, 'Temperature'] > 50,
+        df.loc[i, 'Dewpoint'] > 50
+    ]):
+        print('Delete %s row' % i)
+        df.drop([i], inplace=True)
+
+
+def derive_nth_day_feature(df, feature, N):
+    rows = df.shape[0]
+    nth_prior_measurements = [None] * N + [df[feature][index - N] for index in range(N, rows)]
+    col_name = '{}_{}'.format(feature, N)
+    df[col_name] = nth_prior_measurements
+
+
+# 提取前三天的数据，并将它们与本日的数据合并为一行
+for feature in features:
+    if feature != 'Date':
+        for N in range(1, 4):
+            derive_nth_day_feature(df, feature, N)
+
+spread = df.describe().T  # describe矩阵转置
+IQR = spread['75%'] - spread['25%']  # 计算 interquartile range 四分位距 Q3-Q1
+# 去掉极端数据
+spread['outliers'] = (spread['min'] < (spread['25%'] - (3 * IQR))) | (spread['max'] > (spread['75%'] + 3 * IQR))
+# 显示结果
+print(spread)
+# 删除有空白数据的行
+
+df = df.dropna()
+
+print(df.head())
+print(df.info())
+print(df.describe())
+
+df.to_csv('../data/all_cleaned.csv')
--- tools/combine_csv.py
+++ tools/combine_csv.py
@@ -6,9 +6,11 @@ save_path = r'/Users/lee/Library/CloudStorage/iCloudDrive/Documents/PyCharmProje
 
 file_list = ['1.csv', '2.csv', '3.csv']  # listdir读取出来文件是乱序，手动确认顺序
 
-df = pd.read_csv(folder_path + '/' + file_list[0], usecols=['DATE', 'TMP'])  # 指定cols选择文件中的两列作为DataFrame，忽略其余无关列，节省时间和空间
+features = ['DATE', 'TMP', 'DEW']  # 指定cols选择文件中的三列,忽略其余无关列，节省时间和空间
+
+df = pd.read_csv(folder_path + '/' + file_list[0], usecols=features)
 df.to_csv(save_path, encoding="utf_8_sig", index=False)
 
 for i in range(1, len(file_list)):
-    df = pd.read_csv(folder_path + '/' + file_list[i], usecols=['DATE', 'TMP'])
+    df = pd.read_csv(folder_path + '/' + file_list[i], usecols=features)
     df.to_csv(save_path, encoding="utf_8_sig", index=False, header=False, mode='a+')  # 保存在上一个文件最后一行之后，忽略headers表头
